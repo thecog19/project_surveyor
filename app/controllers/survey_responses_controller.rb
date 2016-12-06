a53cfe6e946c97cbd3976cc67676694b6dffb38c
Francisco Suero <suerofrancisco@yahoo.com>
2016-12-06 18:22:42 -0500

    wooo

--- app/controllers/survey_responses_controller.rb
+++ app/controllers/survey_responses_controller.rb
@@ -7,14 +7,29 @@ class SurveyResponsesController < ApplicationController
   end
 
   def create
-
+    @survey_response = SurveyResponse.new(survey_response_params)
+    @survey_response.save ? successful_create : failed_create
   end
 
   private
+
+    def survey_response_params
+      params.require(:survey_response).permit(:survey_id, submitted_answers_attributes: [:id, :answers])
+    end
 
     def create_responses_and_associate(survey)
       survey.questions.each do |question|
         @survey_response.submitted_answers.build(question: question)
       end
+    end
+
+    def successful_create
+      flash[:success] = ["Question Created"]
+      redirect_to root
+    end
+
+    def failed_create
+      flash.now[:danger] = @question.errors.full_messages
+      render :new
     end
 end
