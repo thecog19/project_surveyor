cea99bf379a51b2ba687e8a79a701c5fc5b83422
Anthony Fuentes <anthony.c.fuentes@gmail.com>
2016-12-06 13:31:04 -0500

    Add destroy dependency to Question possible_answers

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,9 +1,9 @@
 class Question < ApplicationRecord
   validates :question_type_id, presence: true
-  
+
   belongs_to :survey, optional: true
   belongs_to :category, foreign_key: "question_type_id", class_name: "QuestionType"
-  has_many :possible_answers
+  has_many :possible_answers, dependent: :destroy
   has_many :submitted_answers, class_name: "QuestionResponse"
 
   accepts_nested_attributes_for :possible_answers
