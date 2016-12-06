bbc7a98daa85d4f79f417ce9a54c9380a6a8749f
Anthony Fuentes <anthony.c.fuentes@gmail.com>
2016-12-06 14:59:44 -0500

    Begin implementing survey submission

--- app/controllers/survey_responses_controller.rb
+++ app/controllers/survey_responses_controller.rb
@@ -1,6 +1,16 @@
+
 class SurveyResponsesController < ApplicationController
   def new
     @survey = Survey.find(params[:survey_id])
-    @survey_response = SurveyResponse.new
+    @survey_response = @survey.responses.build
+    create_responses_and_associate(@survey)
   end
+
+  private
+
+    def create_responses_and_associate(survey)
+      survey.questions.each do |question|
+        @survey_response.submitted_answers.build(question: question)
+      end
+    end
 end
--- app/models/question_response.rb
+++ app/models/question_response.rb
@@ -2,4 +2,5 @@ class QuestionResponse < ApplicationRecord
   belongs_to :question
   belongs_to :survey_response
   belongs_to :user
+  has_many :possible_answers, through: :question
 end
